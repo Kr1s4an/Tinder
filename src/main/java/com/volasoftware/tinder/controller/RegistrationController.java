9a5b7e16a2e46337aaa062f43d565adfb171cc31
Kr1s4an <krisivratza@gmail.com>
2023-04-10 15:51:37 +0300

    Changes in RegistrationController and UserService

--- src/main/java/com/volasoftware/tinder/controller/RegistrationController.java
+++ src/main/java/com/volasoftware/tinder/controller/RegistrationController.java
@@ -1,18 +1,40 @@
 package com.volasoftware.tinder.controller;
 
 
-import com.volasoftware.tinder.model.UserDto;
+import com.volasoftware.tinder.dto.UserDto;
+import com.volasoftware.tinder.model.Gender;
+import com.volasoftware.tinder.model.User;
+import com.volasoftware.tinder.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.WebRequest;
 
+@Controller
 public class RegistrationController {
+    private final UserService userService;
 
-    @GetMapping("api/v1/users/register")
-    public String registrationForm(WebRequest request, Model model){
-        UserDto userDto = new UserDto();
-        model.addAttribute("user", userDto);
-        return "registration";
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/api/v1/users/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getById(id).get();
+    }
+
+    @PostMapping("/api/v1/users/register")
+    public ResponseEntity registerNewUser(@Valid @RequestBody UserDto userDto) {
+        userService.registerUser(userDto);
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/volasoftware/tinder/service/UserService.java
+++ src/main/java/com/volasoftware/tinder/service/UserService.java
@@ -1,8 +1,9 @@
 package com.volasoftware.tinder.service;
 
+import com.volasoftware.tinder.dto.UserDto;
+import com.volasoftware.tinder.model.Gender;
 import com.volasoftware.tinder.model.User;
 import com.volasoftware.tinder.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,15 +13,24 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<User> getAll(){
         return userRepository.findAll();
     }
 
-    public User save(User user){
-        return userRepository.save(user);
+    public void registerUser(UserDto userDto){
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setPassword(userDto.getPassword());
+        user.setGender(Gender.valueOf(userDto.getGender()));
+        userRepository.save(user);
     }
 
     public Optional<User> getById(Long id){
