a91ba16cd52799e77c2ea05d5155103119710251
Kr1s4an <krisivratza@gmail.com>
2023-04-18 16:00:58 +0300

    (TIN-1) Making changes

--- src/main/java/com/volasoftware/tinder/controller/RegistrationController.java
+++ src/main/java/com/volasoftware/tinder/controller/RegistrationController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class RegistrationController {
     private final UserService userService;
 
