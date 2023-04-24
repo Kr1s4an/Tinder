144bf872432cddda5729247d9e9a6094a9653a10
Kr1s4an <krisivratza@gmail.com>
2023-04-24 09:29:56 +0300

    (TIN-1) First and Last name validation.

--- src/main/java/com/volasoftware/tinder/dto/UserDto.java
+++ src/main/java/com/volasoftware/tinder/dto/UserDto.java
@@ -3,13 +3,19 @@ package com.volasoftware.tinder.dto;
 import com.volasoftware.tinder.annotation.Password;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 
 public class UserDto {
 
     @NotBlank(message = "Name is mandatory")
+    @Pattern(regexp="^[A-Za-z]*$",message = "Must contain only letters")
+    @Size(min = 2, max = 50, message = "The size must be between 2 and 50 letters")
     private String firstName;
     @NotBlank(message = "Name is mandatory")
+    @Pattern(regexp="^[A-Za-z]*$",message = "Must contain only letters")
+    @Size(min = 2, max = 50, message = "The size must be between 2 and 50 letters")
     private String lastName;
     @NotBlank(message = "Password is mandatory")
     @Password
