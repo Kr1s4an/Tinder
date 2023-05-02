454488ce0cb05d05ca191fca77428b88f50341cf
Kr1s4an <krisivratza@gmail.com>
2023-05-02 15:56:48 +0300

    (TIN-16) Foreign key form verification_token table to user table

--- src/main/resources/db/migration/V003__Verification_tokens.sql
+++ src/main/resources/db/migration/V003__Verification_tokens.sql
@@ -1,6 +1,7 @@
 create table verification_tokens(
     ID int not null AUTO_INCREMENT PRIMARY KEY,
-    USER_ID int not null,
+    USER_ID int ,
     TOKEN varchar(50) not null,
-    EXPIRATION_DATE DATETIME DEFAULT CURRENT_TIMESTAMP
+    EXPIRATION_DATE DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (USER_ID) REFERENCES user(ID)
 );
