282ff7fe9cbbe36c9268a58e62ef39c2be6edc89
Kr1s4an <krisivratza@gmail.com>
2023-09-27 10:43:51 +0300

    Jwt token generator class + dependency

---
+++ src/main/java/com/volasoftware/tinder/jwt/JwtTokenGenerator.java
@@ -0,0 +1,24 @@
+package com.volasoftware.tinder.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenGenerator {
+    private static final String SECRET_KEY = "UnbreakableSecretKey";
+    private static final long EXPIRATION_TIME =604800000L; //7 days
+
+    public static String generateToken(String username){
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+
+    }
+}
