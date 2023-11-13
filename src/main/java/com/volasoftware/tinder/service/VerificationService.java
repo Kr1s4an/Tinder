0964b187b952d805babe7eed14ec313eb26489fd
Kr1s4an <krisivratza@gmail.com>
2023-11-13 15:26:00 +0200

    TIN-17 Moving the logic from the controller to the service

--- src/main/java/com/volasoftware/tinder/service/VerificationService.java
+++ src/main/java/com/volasoftware/tinder/service/VerificationService.java
@@ -1,23 +1,38 @@
 package com.volasoftware.tinder.service;
 
 import com.volasoftware.tinder.exception.InvalidVerificationToken;
+import com.volasoftware.tinder.exception.UserAlreadyVerifiedException;
+import com.volasoftware.tinder.exception.UserDoesNotExistException;
 import com.volasoftware.tinder.model.User;
 import com.volasoftware.tinder.model.Verification;
 import com.volasoftware.tinder.repository.UserRepository;
 import com.volasoftware.tinder.repository.VerificationRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class VerificationService {
     private final VerificationRepository verificationRepository;
     private final UserRepository userRepository;
+    private final JavaMailSender mailSender;
+    private final UserService userService;
 
     public VerificationService(VerificationRepository verificationRepository,
-                               UserRepository userRepository) {
+                               UserRepository userRepository,
+                               JavaMailSender mailSender,
+                               UserService userService) {
         this.verificationRepository = verificationRepository;
         this.userRepository = userRepository;
+        this.mailSender = mailSender;
+        this.userService = userService;
     }
 
     public void saveVerificationToken(Verification token) {
@@ -36,5 +51,30 @@ public class VerificationService {
         }
 
         return false;
+    }
+
+    public void resendEmail(String email) throws MessagingException, IOException {
+        User user = userRepository.findOneByEmail(email).orElseThrow(
+                () -> new UserDoesNotExistException("User with this email does not exist")
+        );
+        if (user.isVerified()) {
+            throw new UserAlreadyVerifiedException("User is already verified");
+        }
+        Optional<Verification> oldTokenEntity = verificationRepository.findTokenByUserId(user.getId());
+        oldTokenEntity.ifPresent(verificationRepository::delete);
+
+        Verification newToken = new Verification();
+        newToken.setUser(user);
+        newToken.setToken(UUID.randomUUID().toString());
+        newToken.setCreatedDate(LocalDateTime.now());
+        newToken.setExpirationDate(LocalDateTime.now().plusDays(2));
+        verificationRepository.saveAndFlush(newToken);
+
+        MimeMessage message = mailSender.createMimeMessage();
+        message.setFrom(new InternetAddress("kristinmpetkov@gmail.com"));
+        message.setRecipients(MimeMessage.RecipientType.TO, user.getEmail());
+        message.setSubject("Verification");
+        message.setContent(userService.getEmailContent(newToken.getToken()), "text/html; charset=utf-8");
+        mailSender.send(message);
     }
 }
