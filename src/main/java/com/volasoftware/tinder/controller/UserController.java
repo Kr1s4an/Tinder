1230d22761c599bf13f9b4572a2a97e38a294ee6
kr1s4an <krisivratza@gmail.com>
2023-11-27 11:26:41 +0200

    (TIN-5) Changing he PostMapping annotation to PutMapping

--- src/main/java/com/volasoftware/tinder/controller/UserController.java
+++ src/main/java/com/volasoftware/tinder/controller/UserController.java
@@ -5,10 +5,7 @@ import com.volasoftware.tinder.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -27,7 +24,7 @@ public class UserController {
         return userService.getCurrentUserProfile();
     }
 
-    @PostMapping("/api/v1/users/edit-profile")
+    @PutMapping("/api/v1/users/edit-profile")
     @SecurityRequirement(name = "Bearer Authentication")
     public UserProfileDto editUser(UserProfileDto userProfileDto){
         return userService.editUserProfile(userProfileDto);
