7cab91d7bc6471ad6956087605f071893bff4c73
kr1s4an <krisivratza@gmail.com>
2023-12-04 16:02:13 +0200

    (Testing) Uncommented the custom users

--- src/main/resources/db/migration/V002__Add_users.sql
+++ src/main/resources/db/migration/V002__Add_users.sql
@@ -1,26 +1,26 @@
---insert into user (
---FIRST_NAME,
---LAST_NAME,
---EMAIL,
---PASSWORD,
---GENDER
---) values (
---'Kr1s4an',
---'Petkov',
---'admin@gmail.com',
---'test',
---'MALE'
---);
---insert into user (
---FIRST_NAME,
---LAST_NAME,
---EMAIL,
---PASSWORD,
---GENDER
---) values (
---'A4ko',
---'Maldjanski',
---'slave@gmail.com',
---'nobody',
---'FEMALE'
---);
+insert into user (
+FIRST_NAME,
+LAST_NAME,
+EMAIL,
+PASSWORD,
+GENDER
+) values (
+'Kr1s4an',
+'Petkov',
+'admin@gmail.com',
+'test',
+'MALE'
+);
+insert into user (
+FIRST_NAME,
+LAST_NAME,
+EMAIL,
+PASSWORD,
+GENDER
+) values (
+'A4ko',
+'Maldjanski',
+'slave@gmail.com',
+'nobody',
+'FEMALE'
+);
