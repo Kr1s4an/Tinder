3be913a9127aaec1f4fce8ba528c240cd349242f
kr1s4an <krisivratza@gmail.com>
2023-12-13 15:44:29 +0200

    (TIN-25) Refactoring

--- src/main/java/com/volasoftware/tinder/utility/PasswordGenerator.java
+++ src/main/java/com/volasoftware/tinder/utility/PasswordGenerator.java
@@ -4,9 +4,10 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.passay.EnglishCharacterData;
 
 public class PasswordGenerator {
-    private static final String SYMBOLS = String.valueOf(EnglishCharacterData.Alphabetical);
+    private static final String SYMBOLS = String.valueOf(EnglishCharacterData.Alphabetical.getCharacters());
+    private static final String DIGIT = String.valueOf(EnglishCharacterData.Digit.getCharacters());
 
     public static String generatePassword() {
-        return RandomStringUtils.random(8, SYMBOLS);
+        return RandomStringUtils.random(8, SYMBOLS + DIGIT);
     }
 }
