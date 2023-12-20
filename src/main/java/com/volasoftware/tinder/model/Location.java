cb97a85f81846775dad32a4426c80e204f43222e
kr1s4an <krisivratza@gmail.com>
2023-12-20 10:54:34 +0200

    (TIN-22) Adding new variables into user entity and new enum for the type of the user.

--- src/main/java/com/volasoftware/tinder/model/Location.java
+++ src/main/java/com/volasoftware/tinder/model/Location.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 @Entity
 public class Location {
-
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
--- src/main/java/com/volasoftware/tinder/model/User.java
+++ src/main/java/com/volasoftware/tinder/model/User.java
@@ -33,6 +33,10 @@ public class User extends Auditable<String> implements UserDetails {
     private Role role;
     @ManyToMany(mappedBy = "friends")
     private Set<User> friends;
+    private Integer age;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Location location;
+    private UserType type;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -134,5 +138,29 @@ public class User extends Auditable<String> implements UserDetails {
 
     public void setFriends(Set<User> friends) {
         this.friends = friends;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
     }
 }
