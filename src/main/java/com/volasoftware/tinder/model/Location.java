86a0f64edff07e60847121c85c9dc1ded5905d27
kr1s4an <krisivratza@gmail.com>
2023-12-20 10:39:17 +0200

    (TIN-22) Creating new location entity.

---
+++ src/main/java/com/volasoftware/tinder/model/Location.java
@@ -0,0 +1,48 @@
+package com.volasoftware.tinder.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+    private Double latitude;
+    private Double longitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
